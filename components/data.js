ac3ef38a37641e702a169a3f770e2b8941fc60d8
Jonathan Hong <jonhong777@gmail.com>
2018-10-27 08:03:10 +0000

    Add Pie Chart

--- components/data.js
+++ components/data.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import {PieChart} from 'react-native-chart-kit'
 
 export default class Data extends React.Component {
-    constructor() {
-        super();
-        this.state;
-      }
-    /*
-    componentDidMount() {
-    } 
-    */
-
-    render() {
-        return (
-        <View>
-            <Text>Data</Text>
-        </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <Text>Data Analytics</Text>
+        <PieChart
+          data = {data}
+          width = {screenwidth}
+          height = {200}
+        />
+      </View>
+    );
+  }
 }
-  
