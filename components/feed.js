8e468174c67af38066cecdf5f6d039755d5cab4f
Celine Gauchey <celinegauchey@gmail.com>
2018-10-27 01:35:51 -0400

    started feed

--- components/feed.js
+++ components/feed.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import db from '../firestore';
 
 export default class Feed extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      allPosts: [],
+    };
+    this.allPosts = this.allPosts.bind(this);
+  }
+
+  componentDidMount() {
+    this.allPosts();
+  }
+
+  async allPosts() {
+    const allThePosts = await db.collection('action');
+    const arr = [];
+    await allThePosts.get().then(docsArr => {
+      docsArr.forEach(doc => {
+        arr.push(doc.data());
+      });
+    });
+    this.setState({ allPosts: arr });
+  }
+
   render() {
     return (
       <View>
--- components/form.js
+++ components/form.js
@@ -8,45 +8,52 @@ export default class FormPage extends React.Component {
       action: '',
       quantity: '',
     };
+    this.onSubmit = this.onSubmit.bind(this);
   }
-  updateAction = (newAction) => {
-    this.setState({action: newAction})
+  updateAction = newAction => {
+    this.setState({ action: newAction });
+  };
+  updateQuantity = newQuantity => {
+    this.setState({ quantity: newQuantity });
+  };
+
+  onSubmit() {
+    console.log('Action is: ' + this.state.action);
+    console.log('Quantity is: ' + this.state.quantity);
   }
-  updateQuantity = (newQuantity) => {
-    this.setState({quantity: newQuantity})
-  }
+
   render() {
     return (
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+        }}
+      >
+        <Text>Form</Text>
 
-      <Text>Form</Text>
-
-      <Picker selectedValue={this.state.action} style={{ height: 50, width: 100 }} onValueChange={this.updateAction}>
+        <Picker
+          selectedValue={this.state.action}
+          style={{ height: 50, width: 100 }}
+          onValueChange={this.updateAction}
+        >
           <Picker.Item label="Recycle" value="recycle" />
           <Picker.Item label="Donated Compost" value="donate" />
           <Picker.Item label="Volunteer" value="volunteer" />
-      </Picker>
+        </Picker>
         <Text>Quantity</Text>
         <TextInput
           style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={quantity => this.setState({quantity})}
+          onChangeText={quantity => this.setState({ quantity })}
           value={this.state.quantity}
         />
         <Button
-          onPress={this.handleLogin}
+          onPress={this.onSubmit}
           title="Submit"
           color=""
           accessibilityLabel="Submit Form"
         />
       </View>
     );
-  }
-
-  handleLogin: function(){
-    console.log("Action is: " + this.state.action);
-    console.log("Quantity is: " + this.state.quantity);
   }
 }
