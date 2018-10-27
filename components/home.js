0bbc060491a4a7a044a2e0faaf37830829ded688
Celine Gauchey <celinegauchey@gmail.com>
2018-10-27 03:40:15 -0400

    added photo

--- components/home.js
+++ components/home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
 
 export default class Home extends React.Component {
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              'https://pbs.twimg.com/profile_images/935880969470533632/KprX4QLZ_400x400.jpg',
+          }}
+        />
         <View styles={styles.buttonContainer}>
           <Button
             onPress={() => this.props.navigation.navigate('LogIn')}
@@ -33,5 +39,9 @@ const styles = StyleSheet.create({
   buttonContainer: {
     marginTop: 100,
     backgroundColor: 'white',
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
