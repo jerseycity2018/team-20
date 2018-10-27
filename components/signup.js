e88cabea7a2ae9c23cca80acb44bc6aa6ab615c0
Celine Gauchey <celinegauchey@gmail.com>
2018-10-26 23:00:41 -0400

    fixed brackets

--- components/signup.js
+++ components/signup.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export default class SignUp extends React.Component{
-  constructor(){
+export default class SignUp extends React.Component {
+  constructor() {
     super();
     this.state = {
       email: '',
-      password:'',
-      firstName:'',
-      lastName:'',
-      development:'',
-    }
+      password: '',
+      firstName: '',
+      lastName: '',
+      development: '',
+    };
     this.signUpUser = this.signUpUser.bind(this);
     this.createUser = this.createUser.bind(this);
   }
 
-  async signUpUser(email, password){
+  async signUpUser(email, password) {
     try {
       await firebase.auth().createUserWithEmailAndPassword(email, password);
       const currentUser = await firebase.auth().currentUser;
@@ -24,12 +24,90 @@ export default class SignUp extends React.Component{
     }
   }
 
-  async createUser(id){
-    await db.collection('users').doc(id).set(this.state);
+  async createUser(id) {
+    await db
+      .collection('users')
+      .doc(id)
+      .set(this.state);
   }
 
-  render(){
-    
-  }
+  render() {
+    const navigation = this.props.navigation;
+    return (
+      <Container>
+        <Form>
+          <Item floatingLabel>
+            <Label>First Name</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              clearButtonMode="always"
+              onChangeText={firstName => this.setState({ firstName })}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Username</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              clearButtonMode="always"
+              onChangeText={lastName => this.setState({ lastName })}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              clearButtonMode="always"
+              onChangeText={email => {
+                this.setState({ email });
+              }}
+            />
+          </Item>
+          {!this.state.email.includes('@' && '.') &&
+          this.state.email.length > 0 ? (
+            <Text style={styles.errorMessage}>
+              Please enter a valid email address
+            </Text>
+          ) : null}
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              clearButtonMode="always"
+              secureTextEntry={true}
+              onChangeText={password => {
+                this.setState({ password });
+              }}
+            />
+          </Item>
+          {this.state.password.length < 6 && this.state.password.length > 0 ? (
+            <Text style={styles.errorMessage}>
+              Password must be at least 6 characters
+            </Text>
+          ) : null}
 
+          <Button
+            style={{ marginTop: 10 }}
+            full
+            rounded
+            primary
+            onPress={async () => {
+              const id = await this.signUpUser(
+                this.state.email,
+                this.state.password
+              );
+              this.createUser(id).then(() =>
+                navigation.navigate('EmojiPicker')
+              );
+            }}
+          >
+            <Text style={{ color: 'white' }}>Pick Your Icon</Text>
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
 }
