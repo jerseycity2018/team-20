da188c3ab06781598bb1f8e1fea261c800b49fe1
Celine Gauchey <celinegauchey@gmail.com>
2018-10-27 03:58:47 -0400

    making changes to feed

--- components/styleSheet.js
+++ components/styleSheet.js
@@ -2,7 +2,27 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   feedContainer: {
-    backgroundColor: '#DDF19F',
+    // backgroundColor: '#C0C0C0',
+    flex: 1,
+    paddingBottom: 50,
+    backgroundColor: 'white',
+  },
+  font: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  info: {
+    backgroundColor: '#FEB100',
+    marginBottom: 5,
+    borderRadius: 10,
+    marginLeft: 20,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 17,
+    paddingLeft: 17,
+    width: 300,
   },
 });
 
