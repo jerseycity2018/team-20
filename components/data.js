3f48793453b767eb0df1a2ed123934c523328b8b
Jonathan Hong <jonhong777@gmail.com>
2018-10-27 08:35:56 +0000

    Fix legend text visibility issue

--- components/data.js
+++ components/data.js
@@ -15,35 +15,35 @@ export default class Data extends React.Component {
     };
     const data = [
       {
-        development: 'Adam_1',
+        name: 'Adam_1',
         quantity: 3000,
         color: 'rgba(131, 167, 234, 1)',
         legendFontColor: '#7F7F7F',
         legendFontSize: 15,
       },
       {
-        development: 'Andrew_2',
+        name: 'Andrew_2',
         quantity: 4340,
         color: '#F00',
         legendFontColor: '#7F7F7F',
         legendFontSize: 15,
       },
       {
-        development: 'Bethany_3',
+        name: 'Bethany_3',
         quantity: 412,
         color: 'red',
         legendFontColor: '#7F7F7F',
         legendFontSize: 15,
       },
       {
-        development: 'Woke_1',
+        name: 'Woke_1',
         quantity: 7500,
         color: '#ffffff',
         legendFontColor: '#7F7F7F',
         legendFontSize: 15,
       },
       {
-        development: 'Moscow_1',
+        name: 'Moscow_1',
         quantity: 800,
         color: 'rgb(0, 0, 255)',
         legendFontColor: '#7F7F7F',
