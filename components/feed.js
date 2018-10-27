9a3548cb389d82d3ee3fd4a37f25dfd32f48d479
Celine Gauchey <celinegauchey@gmail.com>
2018-10-27 01:03:33 -0400

    feed is showing up

--- components/feed.js
+++ components/feed.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class Feed extends React.Component {
   render() {
     return (
       <View>
         <Text>Feed</Text>
+        <Image
+          style={{ width: 66, height: 58 }}
+          source={{
+            uri:
+              'https://images-na.ssl-images-amazon.com/images/I/71WGWTtJfIL._SX425_.jpg',
+          }}
+        />
       </View>
     );
   }
