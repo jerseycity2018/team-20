da8054a22ec9442d1d0205a35e7a1a7528df8d47
Celine Gauchey <celinegauchey@gmail.com>
2018-10-27 03:13:06 -0400

    picker working

--- components/home.js
+++ components/home.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default class Home extends React.Component {
   render() {
     return (
       <View>
         <Text>Home</Text>
-        <Button
-          onPress={() => this.props.navigation.navigate('LogIn')}
-          title="Log In"
-        />
-        <Button
-          onPress={() => this.props.navigation.navigate('SignUp')}
-          title="Sign Up"
-        />
-        <Button
-          onPress={() => this.props.navigation.navigate('Form')}
-          title="Form"
-        />
-        <Button
-          onPress={() => this.props.navigation.navigate('Feed')}
-          title="Feed"
-        />
+        <View styles={styles.buttonContainer}>
+          <Button
+            onPress={() => this.props.navigation.navigate('LogIn')}
+            title="Log In"
+          />
+          <Button
+            onPress={() => this.props.navigation.navigate('SignUp')}
+            title="Sign Up"
+          />
+          <Button
+            onPress={() => this.props.navigation.navigate('Form')}
+            title="Form"
+          />
+          <Button
+            onPress={() => this.props.navigation.navigate('Feed')}
+            title="Feed"
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 100,
+    backgroundColor: 'white',
+  },
+});
