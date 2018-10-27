8cf98489909849d15feb01f6b804b73277634178
Jonathan Hong <jonhong777@gmail.com>
2018-10-27 08:06:44 +0000

    Add chart configurations

--- components/data.js
+++ components/data.js
@@ -3,6 +3,11 @@ import { View, Text } from 'react-native';
 import {PieChart} from 'react-native-chart-kit'
 
 export default class Data extends React.Component {
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientTo: '#08130D',
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
+  }
   render() {
     return (
       <View>
